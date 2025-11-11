dbaeb696ba16cafcbc35ebe003cb23f3cfd972ba
CScott Brown <gitpushoriginmaster@gmail.com>
2025-11-11 09:42:38 -0600

    fixing non_max_supression import from ultralytics refactor

--- hierarchical_yolo/yolo_utils.py
+++ hierarchical_yolo/yolo_utils.py
@@ -1,6 +1,10 @@
 from hierarchical_loss.tree_utils import get_roots
 from hierarchical_loss.path_utils import optimal_hierarchical_paths
 import ultralytics
+try:
+    from ultralytics.utils.ops import non_max_suppression # old version
+except:
+    from ultralytics.utils.nms import non_max_suppression # new version
 import torch
 from PIL import Image
 
@@ -105,7 +109,7 @@ def postprocess_raw_output(
     # TODO: cache this
     roots = get_roots(hierarchy)
     # TODO: this logic is wrong.  We need to subset raw_yolo_output to root classes instead of using the `classes` arg
-    _, nms_idxs = ultralytics.utils.nms.non_max_suppression(raw_yolo_output, classes=roots, return_idxs=True, iou_thres=nms_iou_thres, conf_thres=nms_conf_thres, multi_label=True)
+    _, nms_idxs = non_max_suppression(raw_yolo_output, classes=roots, return_idxs=True, iou_thres=nms_iou_thres, conf_thres=nms_conf_thres, multi_label=True)
     for i, idx in enumerate(nms_idxs):
         flat_idx = idx.flatten().long()
         nms_output = raw_yolo_output[i].index_select(1, flat_idx)
