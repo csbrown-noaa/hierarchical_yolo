eff632ec07447f1388049e6fec3ab1a5599732de
CScott Brown <gitpushoriginmaster@gmail.com>
2026-01-14 10:18:37 -0600

    adding coco loading logic

--- hierarchical_yolo/mscoco_hierarchical_model.py
+++ hierarchical_yolo/mscoco_hierarchical_model.py
@@ -1,124 +1,74 @@
 from .hierarchical_detection import HierarchicalDetectionTrainer
 from .yolo_utils import get_yolo_class_names
 import yaml
+import json
 from importlib import resources
 from hierarchical_loss.hierarchy import Hierarchy
 
-COCO_HIERARCHY = {
-    'living_thing': 'object',
-    'person': 'living_thing',
-    'animal': 'living_thing',
-    'domestic_animal': 'animal',
-    'cat': 'domestic_animal',
-    'dog': 'domestic_animal',
-    'bird': 'domestic_animal',
-    'livestock_animal': 'animal',
-    'horse': 'livestock_animal',
-    'sheep': 'livestock_animal',
-    'cow': 'livestock_animal',
-    'wild_animal': 'animal',
-    'elephant': 'wild_animal',
-    'bear': 'wild_animal',
-    'zebra': 'wild_animal',
-    'giraffe': 'wild_animal',
-    'vehicle': 'object',
-    'land_vehicle': 'vehicle',
-    'bicycle': 'land_vehicle',
-    'car': 'land_vehicle',
-    'motorcycle': 'land_vehicle',
-    'bus': 'land_vehicle',
-    'train': 'land_vehicle',
-    'truck': 'land_vehicle',
-    'airplane': 'vehicle',
-    'boat': 'vehicle',
-    'outdoor_and_sports_object': 'object',
-    'street_fixture': 'outdoor_and_sports_object',
-    'traffic light': 'street_fixture',
-    'fire hydrant': 'street_fixture',
-    'stop sign': 'street_fixture',
-    'parking meter': 'street_fixture',
-    'bench': 'street_fixture',
-    'sports_equipment': 'outdoor_and_sports_object',
-    'ball_sport_equipment': 'sports_equipment',
-    'sports ball': 'ball_sport_equipment',
-    'baseball bat': 'ball_sport_equipment',
-    'baseball glove': 'ball_sport_equipment',
-    'tennis racket': 'ball_sport_equipment',
-    'board_sport_equipment': 'sports_equipment',
-    'skateboard': 'board_sport_equipment',
-    'surfboard': 'board_sport_equipment',
-    'snowboard': 'board_sport_equipment',
-    'other_sports_equipment': 'sports_equipment',
-    'frisbee': 'other_sports_equipment',
-    'skis': 'other_sports_equipment',
-    'kite': 'other_sports_equipment',
-    'indoor_object': 'object',
-    'furniture': 'indoor_object',
-    'chair': 'furniture',
-    'couch': 'furniture',
-    'potted plant': 'furniture',
-    'bed': 'furniture',
-    'dining table': 'furniture',
-    'appliance_and_electronics': 'indoor_object',
-    'appliance': 'appliance_and_electronics',
-    'microwave': 'appliance',
-    'oven': 'appliance',
-    'toaster': 'appliance',
-    'sink': 'appliance',
-    'refrigerator': 'appliance',
-    'toilet': 'appliance',
-    'electronics': 'appliance_and_electronics',
-    'tv': 'electronics',
-    'laptop': 'electronics',
-    'mouse': 'electronics',
-    'remote': 'electronics',
-    'keyboard': 'electronics',
-    'cell phone': 'electronics',
-    'kitchenware': 'indoor_object',
-    'bottle': 'kitchenware',
-    'wine glass': 'kitchenware',
-    'cup': 'kitchenware',
-    'fork': 'kitchenware',
-    'knife': 'kitchenware',
-    'spoon': 'kitchenware',
-    'bowl': 'kitchenware',
-    'personal_care_item': 'indoor_object',
-    'hair dryer': 'personal_care_item',
-    'toothbrush': 'personal_care_item',
-    'accessory_and_item': 'object',
-    'personal_accessory': 'accessory_and_item',
-    'backpack': 'personal_accessory',
-    'umbrella': 'personal_accessory',
-    'handbag': 'personal_accessory',
-    'tie': 'personal_accessory',
-    'suitcase': 'personal_accessory',
-    'home_and_office_item': 'accessory_and_item',
-    'book': 'home_and_office_item',
-    'clock': 'home_and_office_item',
-    'vase': 'home_and_office_item',
-    'scissors': 'home_and_office_item',
-    'teddy bear': 'home_and_office_item',
-    'food': 'object',
-    'fruit': 'food',
-    'banana': 'fruit',
-    'apple': 'fruit',
-    'orange': 'fruit',
-    'vegetable': 'food',
-    'broccoli': 'vegetable',
-    'carrot': 'vegetable',
-    'prepared_meal': 'food',
-    'sandwich': 'prepared_meal',
-    'hot dog': 'prepared_meal',
-    'pizza': 'prepared_meal',
-    'dessert': 'food',
-    'donut': 'dessert',
-    'cake': 'dessert'
-}
 
 YOLO_DATASET_YAML = resources.files('hierarchical_yolo.models.coco128').joinpath('hierarchicalcoco128.yaml')
 with open(YOLO_DATASET_YAML, 'r') as f:
     COCO_YOLO_ID_MAP = get_yolo_class_names(f)
+COCO_HIERARCHY_JSON = resources.files('hierarchical_yolo.models').joinpath('coco_hierarchy.json')
+with open(COCO_HIERARCHY_JSON, 'r') as f:
+    COCO_HIERARCHY = json.load(f)
 
 class MSCOCOHierarchicalDetectionTrainer(HierarchicalDetectionTrainer):
     # Hierarchy requires the index -> name map in the other direction
     _hierarchy = Hierarchy(COCO_HIERARCHY, {v: k for k,v in COCO_YOLO_ID_MAP.items()})
+
+def prepare_coco_data(destination_directory: str) -> None:
+    import pycocowriter.cocomerge
+    import pycocowriter.coco2yolo
+    import os
+    import urllib.request
+    import zipfile
+    DATA = os.path.join(destination_directory, 'hierarchical_coco')
+    os.makedirs(DATA, exist_ok = True)
+    COCO_ANNOTATIONS_URL = 'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
+    COCO_TRAIN_IMAGES_URL = 'http://images.cocodataset.org/zips/train2017.zip'
+    COCO_TEST_IMAGES_URL = 'http://images.cocodataset.org/zips/test2017.zip'
+    COCO_VAL_IMAGES_URL = 'http://images.cocodataset.org/zips/val2017.zip'
+    # paths to data
+    ANNOTATIONS = os.path.join(DATA, 'annotations', 'instances')
+    TRAIN_ANNOTATIONS = os.path.join(ANNOTATIONS, 'instances_train2017.json')
+    TEST_ANNOTATIONS = os.path.join(ANNOTATIONS, 'instances_test2017.json')
+
+    # download and unzip everything
+    for url in [
+        COCO_ANNOTATIONS_URL,
+        COCO_TRAIN_IMAGES_URL,
+        COCO_TEST_IMAGES_URL,
+        COCO_VAL_IMAGES_URL
+    ]:
+        filename = url.split('/')[-1]
+        destination = os.path.join(DATA, filename)
+        urllib.request.urlretrieve(url, destination)
+        with zipfile.ZipFile(destination, "r") as f:
+            f.extractall(DATA)
+        os.remove(destination)
+
+    # load the hierarchy
+    COCO_HIERARCHY_JSON = resources.files('hierarchical_yolo.models').joinpath('coco_hierarchy.json')
+    with open(COCO_HIERARCHY_JSON, 'r') as f:
+        COCO_HIERARCHY = json.load(f)
+    all_category_names = sorted(COCO_HIERARCHY.keys() | COCO_HIERARCHY.values())
+    all_categories = [
+        { 
+            'id': i,
+            'name': cat 
+        } for i, cat in enumerate(all_category_names)
+    ]
+
+    # update the files to have the new categories
+    for coco_file in [TEST_ANNOTATIONS, TRAIN_ANNOTATIONS]:
+        with open(coco_file, 'r') as f:
+            coco = json.load(f)
+        expanded_coco = pycocowriter.cocomerge.coco_merge(coco, all_categories)
+        expanded_coco = pycocowriter.cocomerge.coco_collapse_categories(expanded_coco)
+        expanded_coco = pycocowriter.cocomerge.coco_reindex_categories(expanded_coco)
+        with open(coco_file, 'w') as f:
+            json.dump(expanded_coco, f)
+
+    # create yolo-compatible annotations
+    pycocowriter.coco2yolo.coco2yolo(ANNOTATIONS, DATA) 
